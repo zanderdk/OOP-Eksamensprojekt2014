0a9176bbec8968f2b1af2cd174480c2f63f58513
Frederik Andersen <fhan13@student.aau.dk>
2014-04-03 21:21:25 +0200

    hmm

---
+++ OOP-EksamensOpgave2014/OOP-EksamensOpgave2014/RegistreringnummerException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP_EksamensOpgave2014
+{
+    class RegistreringnummerException : Exception
+    {
+        public RegistreringnummerException()
+            : base() {}
+        public RegistreringnummerException(string s)
+            : base(s) {}
+        public RegistreringnummerException(string s, Exception ex)
+            : base(s, ex) {}
+    }
+
+}
---
+++ OOP-EksamensOpgave2014/OOP-EksamensOpgave2014/StortKøretøj.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_EksamensOpgave2014
+{
+    abstract public class StortKøretøj : Køretøj
+    {
+        public double Højde;
+        public double Længde;
+        public double Vægt;
+
+        protected override double MinMotorstørrelse
+        {
+            get { return 4.2; }
+        }
+
+        protected override double MaxMotorstørrelse
+        {
+            get { return 15; }
+        }
+    }
+}
