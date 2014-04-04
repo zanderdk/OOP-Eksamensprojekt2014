08f41cb7731b86e2139b6bcdf529d0c7d3a2f4c7
Frederik Andersen <fhan13@student.aau.dk>
2014-04-04 14:04:58 +0200

    så kan den compile

--- OOP-EksamensOpgave2014/OOP-EksamensOpgave2014/Program.cs
+++ OOP-EksamensOpgave2014/OOP-EksamensOpgave2014/Program.cs
@@ -6,7 +6,7 @@ namespace OOP_EksamensOpgave2014
     {
         static void Main()
         {
-            var bil = new Privatbil(Bændstof.Benzin, 2013) { Registreringsnummer = "aa12345", Navn = "Volvo", Km = 9001, KmPrL = 14};
+            var bil = new Privatbil(Brændstof.Benzin, 2013) { Registreringsnummer = "aa12345", Navn = "Volvo", Km = 9001, KmPrL = 14};
             Console.WriteLine(bil.ToString());
             Console.WriteLine(bil.Registreringsnummer);
             Console.WriteLine(bil.Kørekorttype);
