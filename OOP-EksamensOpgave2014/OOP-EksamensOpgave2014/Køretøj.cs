30f2e5c9169c4aebf82b7f341745c98c13582863
Frederik Andersen <fhan13@student.aau.dk>
2014-04-07 14:52:45 +0200

    så nu virker det

--- OOP-EksamensOpgave2014/OOP-EksamensOpgave2014/Køretøj.cs
+++ OOP-EksamensOpgave2014/OOP-EksamensOpgave2014/Køretøj.cs
@@ -5,8 +5,7 @@ namespace OOP_EksamensOpgave2014
 {
     public abstract class Køretøj
     {
-
-        public Køretøj(string registreringsnummer, string navn , Brændstof brændstof, double motorStørelse, int årgang)
+        protected Køretøj(string registreringsnummer, string navn , Brændstof brændstof, double motorStørelse, int årgang)
         {
             Navn = navn;
             Registreringsnummer = registreringsnummer;
